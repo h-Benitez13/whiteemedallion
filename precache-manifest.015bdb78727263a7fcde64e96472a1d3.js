b9808a78762f0dc56c1722b8b4e496e08fc9c0cd
h-Benitez13 <hbenitez3711@gmail.com>
2019-07-04 17:16:25 -0400

    Updates

---
+++ precache-manifest.015bdb78727263a7fcde64e96472a1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df557944ef1acd71cda0e20fb18c3980",
+    "url": "/whiteemedallion/index.html"
+  },
+  {
+    "revision": "02988bf5d0a8d54c3cff",
+    "url": "/whiteemedallion/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/whiteemedallion/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "02988bf5d0a8d54c3cff",
+    "url": "/whiteemedallion/static/js/main.4c318e33.chunk.js"
+  },
+  {
+    "revision": "3dd3d2e4e1741d428aad",
+    "url": "/whiteemedallion/static/js/runtime~main.2fb64930.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/whiteemedallion/static/media/logo.5d5d9eef.svg"
+  }
+]);
