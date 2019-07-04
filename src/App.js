989bebdd8d71f9315f31245ee289dc91d5b82b1b
h-Benitez13 <hbenitez3711@gmail.com>
2019-07-04 17:19:19 -0400

    gitignore added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -19,6 +19,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <iframe
+        title="clock title"
+        src="http://free.timeanddate.com/countdown/i6u1vmc7/n137/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pcfff/tcfff/fs100/szw320/szh135/tatTime%20left%20to%20Event%20in/tac000/tptTime%20since%20Event%20started%20in/tpc000/mac000/mpc000/iso2020-07-04T00:00:00"
+        allowTransparency="true"
+        frameborder="0"
+        width="181"
+        height="69"
+      />
     </div>
   );
 }
