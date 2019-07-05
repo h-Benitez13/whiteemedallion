9fc4245d5759ae96b02025d65b83bb657f1ee22b
h-Benitez13 <hbenitez3711@gmail.com>
2019-07-04 21:58:52 -0400

    we live

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ export const App = () => {
       <Countdown date={Date.now() + 31547000000} renderer={renderTime} />
       <div className="App vh-100">
         <video
-          className="fr mr6"
+          className="fr mr6 mt3"
           src={Video1}
           width="300"
           height="400"
           controls
           autoPlay
+          loop
         />
       </div>
     </renderTime>
