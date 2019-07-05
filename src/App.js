8bd8ff449d90637645e99824fdd5c5d7b2a2997d
h-Benitez13 <hbenitez3711@gmail.com>
2019-07-04 21:50:26 -0400

    readjusting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import renderTime from "./Countdown";
 import Countdown from "react-countdown-now";
+import Video1 from "./Vids/vid.mov";
 import "./App.css";
 
 export const App = () => {
   return (
     <renderTime>
       <Countdown date={Date.now() + 31547000000} renderer={renderTime} />
-      <div className="App vh-100" />
+      <div className="App vh-100">
+        <video
+          className="fr mr6"
+          src={Video1}
+          width="300"
+          height="400"
+          controls
+          autoPlay
+        />
+      </div>
     </renderTime>
   );
 };
--- src/Countdown.js
+++ src/Countdown.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function renderTime({ days, hours, minutes, seconds, completed }) {
   const Completionist = () => (
-    <h1 className="fl w-100 f1 mb1 ml6 pl4"> WE LIVE BABY!</h1>
+    <h1 className="fl w-100 f1 mt4 ml6 pl4"> WE LIVE BABY!</h1>
   );
   if (completed) {
     return <Completionist />;
   } else {
     return (
-      <div>
+      <div className="mt5">
         <h1 className="dib fl w-100 f1  ">
           {days}: {hours}: {minutes}: {seconds}
         </h1>
